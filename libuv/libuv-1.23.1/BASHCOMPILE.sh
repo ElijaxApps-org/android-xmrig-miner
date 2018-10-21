0471fda442910452f859577f1fe8716eae5e54c4
ElijaxApps <elijaxapps@gmail.com>
2018-10-21 21:03:08 +0200

    Fdroid build

--- libuv/libuv-1.23.1/BASHCOMPILE.sh
+++ libuv/libuv-1.23.1/BASHCOMPILE.sh
@@ -4,25 +4,26 @@ WD=$(dirname pwd)
 BASEDIR=$(dirname "$0")
 cd ${BASEDIR}
 
+export NDK=/usr/lib/android-ndk/
+
 rm -fr out arm arm64 x86 x86_64 android-toolchain-**
 
 source android-configure-arm ${NDK} gyp 23
+BUILDTYPE=Release make -C out
 mv -f out arm
-BUILDTYPE=Release make -C arm
 
 
 source android-configure-arm64 ${NDK} gyp 23
+BUILDTYPE=Release make -C out
 mv -f out arm64
-BUILDTYPE=Release make -C arm64
-
 
 source android-configure-x86 ${NDK} gyp 23
+BUILDTYPE=Release make -C out
 mv -f out x86
-BUILDTYPE=Release make -C x86
 
 source android-configure-x86_64 ${NDK} gyp 23
+BUILDTYPE=Release make -C out
 mv -f out x86_64
-BUILDTYPE=Release make -C x86_64
 
 cd ${WD}
 
